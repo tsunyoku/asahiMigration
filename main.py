39c3de083ad3a32522f267143ab860eeb91d10a1
Iteki <contact@iteki.pw>
2021-07-04 13:18:36 +0000

    more update stuff

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from cmyui import AsyncSQLPool, log
-from objects import glob
+from cmyui import AsyncSQLPool, log, Ansi
+from pathlib import Path
+
 from constants.privs import gulagPrivileges, asahiPrivileges
+from objects import glob
 
 import asyncpg
 import asyncio
 import uvloop
+import os
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
@@ -13,10 +16,32 @@ async def startup():
 
     glob.sql = AsyncSQLPool()
     await glob.sql.connect(glob.config.gulag_sql)
-    log('Connected to MySQL!')
+    log('Connected to MySQL (gulag)!')
 
     glob.postgres = await asyncpg.connect(**glob.config.asahi_postgres)
-    log('Connected to PostgreSQL!')
+    log('Connected to PostgreSQL (Asahi)!')
+    
+async def close_dbs():
+    await glob.sql.close()
+    await glob.postgres.close()
+    
+async def import_db():
+    db_file = Path.cwd() / 'ext/db.sql'
+    
+    if not db_file.exists():
+        log("Couldn't find database file. Please ensure you cloned correctly and try again...", Ansi.LRED)
+
+    log('Importing database. Note: You may be asked to enter your password, please do so!')
+        
+    # if they're running it again after first time then there's probably an error, lets just drop the database for safety
+    # we need to restart postgres first incase of any dead sessions
+    os.system('sudo /etc/init.d/postgresql restart >/dev/null 2>&1')
+    os.system(f'sudo -u postgres -i psql -c "DROP DATABASE {glob.config.asahi_postgres["database"]};" >/dev/null 2>&1')
+    os.system(f'sudo -u postgres -i psql -c "CREATE DATABASE {glob.config.asahi_postgres["database"]};" >/dev/null 2>&1')
+
+    os.system(f'sudo -u postgres -i psql {glob.config.asahi_postgres["database"]} < {str(db_file)} >/dev/null 2>&1')
+    
+    log('Database imported!')
 
 async def convert_priv(old_priv):
     new_priv = asahiPrivileges(0)
@@ -46,25 +71,7 @@ async def convert_priv(old_priv):
     return new_priv
 
 async def convert_users():
-    gulag_users = await glob.sql.fetchall(f'SELECT * FROM users')
-
-    # create users db
-    await glob.postgres.execute(
-        'CREATE TABLE public.users ('
-        'id integer NOT NULL,'
-        'name character varying(16) NOT NULL,'
-        "email character varying(254) DEFAULT ''::character varying NOT NULL,"
-        'pw text NOT NULL,'
-        "country character varying(2) DEFAULT 'xx'::character varying NOT NULL,"
-        'priv integer DEFAULT 1 NOT NULL,'
-        'safe_name character varying(16) NOT NULL,'
-        'clan integer DEFAULT 0 NOT NULL,'
-        'freeze_timer bigint DEFAULT 0 NOT NULL,'
-        'registered_at bigint NOT NULL,'
-        'silence_end bigint DEFAULT 0 NOT NULL,'
-        'donor_end bigint DEFAULT 0 NOT NULL'
-        ');'
-    )
+    gulag_users = await glob.sql.fetchall('SELECT * FROM users')
 
     for user in gulag_users:
         id = user['id']
@@ -102,13 +109,32 @@ async def convert_users():
     await glob.postgres.execute("ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);")
 
     log('All users converted!')
+    
+async def convert_stats(): # TODO: finish this & the rest when im not lazy and have some final things added to asahi
+    gulag_stats = await glob.sql.fetchall('SELECT * FROM stats')
+    
+    for user in gulag_stats:
+        id = user['id']
+
+        tscore_std = user['tscore_vn_std']
+        tscore_taiko = user['tscore_vn_taiko']
+        tscore_catch = user['tscore_vn_catch']
+        tscore_mania = user['tscore_vn_mania']
+
+        rscore_std = user['rscore_vn_std']
+        rscore_taiko = user['rscore_vn_taiko']
+        rscore_catch = user['rscore_vn_catch']
+        rscore_mania = user['rscore_vn_mania']
 
 input(
-    'Please be aware that before running this script, you should have made a blank database in PostgreSQL for Asahi and have a valid gulag database in MySQL.'
-    'If not, please sort that now and run again (Ctrl + C to exit).'
+    'Please be aware that before running this script, you should have made a blank database in PostgreSQL for Asahi and have a valid gulag database in MySQL. '
+    'If not, please sort that now and run again (Ctrl + C to exit). '
     'Otherwise, press any key to continue!'
 )
 
 loop = asyncio.get_event_loop()
+loop.run_until_complete(import_db())
 loop.run_until_complete(startup())
 loop.run_until_complete(convert_users())
+loop.run_until_complete(close_dbs())
+log('Migration complete!')
